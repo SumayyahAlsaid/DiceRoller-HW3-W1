ee7f5ef32d16708ebc455d8695b71c7529808955
SumayyahAlsaid <sumayyahalsaid@gmail.com>
2022-08-04 17:48:15 +0300

    Dice Roller HW3 Lab 5 - Tuwaiq Academy

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.diceroller
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +16,32 @@ class MainActivity : AppCompatActivity() {
 
         // declare two variables for button and text
         val rollButton : Button = findViewById(R.id.btn_roll)
-        val numberText : TextView = findViewById(R.id.txt_number)
 
-        // function for event toast when on click button
         rollButton.setOnClickListener {
-
-            // declare variable for toast
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-
-            // function for show toast
-            toast.show()
+            rollDice()
         }
+    }
+
+    //Function for roll the dice and update the screen with the result.
+    private fun rollDice() {
+        // create object
+        val dice = Dice(6)
+
+        // save the result number
+        val diceRoll = dice.roll()
+
+        // reference to imageView
+        val imageDice : ImageView = findViewById(R.id.image_dice)
+
+        // check number of dice roll and update screen (UI) by image
+        when (diceRoll) {
+            1 -> imageDice.setImageResource(R.drawable.dice_1)
+            2 -> imageDice.setImageResource(R.drawable.dice_2)
+            3 -> imageDice.setImageResource(R.drawable.dice_3)
+            4 -> imageDice.setImageResource(R.drawable.dice_4)
+            5 -> imageDice.setImageResource(R.drawable.dice_5)
+            6 -> imageDice.setImageResource(R.drawable.dice_6)
+        }
+
     }
 }
